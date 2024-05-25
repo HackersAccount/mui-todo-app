edd2e8f7f448689689abd30963bb7ac2ece73ef7
Munashe Njanji <munashenjanji45@gmail.com>
2024-05-25 20:53:58 +0200

    Update TodoItem to Trigger the Confirmation Modal

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,12 +3,12 @@ import { ListItem, ListItemText, IconButton, Checkbox } from "@mui/material";
 import { DeleteOutline } from "@mui/icons-material";
 
 
-const TodoItem = ({ todo, index, toggleTodo, deleteTodo }) => {
+const TodoItem = ({ todo, index, toggleTodo, confirmDelete }) => {
     return(
         <ListItem
             style={{textDecoration: todo.completed ? "line-through" : "none"}}
             secondaryAction={
-                <IconButton edge="end" aria-label="delete" onClick={() => deleteTodo(index)}>
+                <IconButton edge="end" aria-label="delete" onClick={() => confirmDelete(index)}>
                     <DeleteOutline />
                 </IconButton> 
             }
